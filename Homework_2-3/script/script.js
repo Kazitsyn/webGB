eb92edf6ad944f55aa141008f9eb88782cf151b4
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2023-05-09 21:22:57 +0300

    add hw2,3

---
+++ Homework_2-3/script/script.js
@@ -0,0 +1,21 @@
+
+function validateEmail(email) {
+    var pattern  = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return pattern .test(email);
+  }
+  
+  function validate() {
+    var $result = $("#result");
+    var email = $("#email").val();
+    $result.text("");
+  
+    if (validateEmail(email)) {
+      $result.text(email + " подтвержден.");
+    } else {
+      $result.text(email + " не правильный email.");
+    }
+    return false;
+}
+
+$("#validate").bind("click", validate);
+
